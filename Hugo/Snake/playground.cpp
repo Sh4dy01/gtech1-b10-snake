84d5e4f7ada87c86afa49e28ea0da9b86c95b482
Hugo MAESTRACCI <h.maestracci83@gmail.com>
2022-01-24 16:04:20 +0100

    Added playground class

---
+++ Hugo/Snake/playground.cpp
@@ -0,0 +1,8 @@
+#include "playground.hpp"
+
+Playground::Playground(){
+    this->nbrSquare = 20;
+    this->pixels = 32;
+    this->width = nbrSquare * pixels;
+    this->height = width;
+}
---
+++ Hugo/Snake/playground.hpp
@@ -0,0 +1,11 @@
+#include "renderer.cpp"
+
+//Grille de 20 carrés de 32*32
+class Playground{
+    public:
+    Playground();
+    ~Playground();
+    
+    private:
+    int width, height, nbrSquare, pixels;
+};
