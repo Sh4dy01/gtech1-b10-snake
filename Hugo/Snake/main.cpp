957a769e51a71c7a88a337eb03b2b0c8ba02312b
Hugo MAESTRACCI <h.maestracci83@gmail.com>
2022-01-25 20:54:19 +0100

    Added playground

--- Hugo/Snake/main.cpp
+++ Hugo/Snake/main.cpp
@@ -1,42 +1,23 @@
-#include "renderer.cpp"
+#include "playground.cpp"
 
 int main()
 {
-    bool IsGameRunning = true;
-
     MainSDLWindow main_window;
-    main_window.Init("Snake", 640, 640);
-    
+    main_window.Init();
+    int frame_rate = main_window.GetFrameRate();
     SDL_Renderer* renderer = main_window.GetRenderer();
 
     do{
-
         Uint32 frame_time_start = SDL_GetTicks();
-        SDL_Event event;
 
-        const Uint8 *keystates = SDL_GetKeyboardState(NULL);
-
-        while(SDL_PollEvent(&event)){
-            if(event.type == SDL_QUIT){
-                IsGameRunning = false;
-            }
-        }
+        main_window.CheckForQuit(); //Click X to quit the game
+        main_window.DrawPlayground(); //Draw the playground
         
-        //Déplacement du rect
-
-        //Clear
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); //Change la couleur de fond
-        SDL_RenderClear(renderer);
-
-        //Draw a white simple rectangle
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-        
-        //Render the rectangle
-        SDL_RenderPresent(renderer);
+        SDL_RenderPresent(renderer); //Refresh the renderer
 
         Uint32 frame_time_interval = SDL_GetTicks() - frame_time_start;
         if(frame_time_interval < frame_rate){
             SDL_Delay(frame_rate-frame_time_interval);
         }
-    }while(IsGameRunning);
+    }while(main_window.GetGameState());
 }
--- Hugo/Snake/playground.cpp
+++ Hugo/Snake/playground.cpp
@@ -1,8 +1,65 @@
 #include "playground.hpp"
 
-Playground::Playground(){
+MainSDLWindow::MainSDLWindow(){
+    this->window,renderer = NULL;
+    
     this->nbrSquare = 20;
     this->pixels = 32;
     this->width = nbrSquare * pixels;
     this->height = width;
+    this->IsGameRunning = true;
+    this->frame_rate = 20;
+
+    this->rect.h = width - pixels * 2;
+    this->rect.w = rect.h;
+    this->rect.x = pixels;
+    this->rect.y = rect.x;
 }
+
+MainSDLWindow::~MainSDLWindow(){
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int MainSDLWindow::Init(){
+    //Création de la fenêtre
+    window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_RESIZABLE);
+    if (window == NULL){
+        cout << "Erreur lors de la creation de la fenêtre:" << SDL_GetError();
+        return EXIT_FAILURE;
+    }
+    
+    //Création du renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL){
+        cout << "Erreur lors de la creation du rendu:" << SDL_GetError();
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+//Draw the playground
+void MainSDLWindow::DrawPlayground(){
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 75, 75, 75, SDL_ALPHA_OPAQUE);
+    SDL_RenderFillRect(renderer, &rect);
+}
+
+//Quit the game
+void MainSDLWindow::CheckForQuit(){
+    SDL_Event event;
+    const Uint8 *keystates = SDL_GetKeyboardState(NULL);
+
+    while(SDL_PollEvent(&event)){
+        if(event.type == SDL_QUIT){
+            IsGameRunning = false;
+        }
+    }
+}
+
+SDL_Renderer *MainSDLWindow::GetRenderer(){return renderer;}
+bool MainSDLWindow::GetGameState(){return IsGameRunning;}
+int MainSDLWindow::GetFrameRate(){return frame_rate;}
